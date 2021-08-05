523dfd9000762f09bf4de477e7137f70ddb93d22
Kaia <kbergmanndean@gmail.com>
2021-08-05 15:46:56 -0400

    added dots

--- src/Components/Artists.js
+++ src/Components/Artists.js
@@ -5,9 +5,13 @@ function Artists({artists,setArtists}){
     return(
         <div className="artists-page">
         <Navbar/>
+        <div className="circle"></div>
+        <div className="circle-2"></div>
+        <div className="circle-3"></div>
         <div className="artist-list-page">
         <ul id="artist-list">{artists.map((artist)=>{return<div><a className="artist" href={`/artists/${artist.id}`} key={artist.id}>{artist.name}</a><br/></div>})}</ul>
         </div>
+        {/* <div className="circle"></div> */}
         </div>
     )
 
--- src/Components/Museums.js
+++ src/Components/Museums.js
@@ -4,6 +4,9 @@ function Museums({museums,setMuseums}){
     return(
         <div className="museums-page">
         <Navbar/>
+        <div className="circle"></div>
+        <div className="circle-2"></div>
+        <div className="circle-3"></div>
         <div className="museums-list-page">
         <ul id="museum-list">{museums.map((museum)=>{return<div><a className="museum" href={`/museums/${museum.id}`} key={museum.id}>{museum.name}</a><br/></div>})}</ul>
         </div>
