748fe65ae94745e9ec3f1f8c92fe943d9621458c
Kaia <kbergmanndean@gmail.com>
2021-08-06 10:42:04 -0400

    more styling

--- src/Components/Artists.js
+++ src/Components/Artists.js
@@ -9,6 +9,7 @@ function Artists({artists,setArtists}){
         <div className="circle-2"></div>
         <div className="circle-3"></div>
         <div className="artist-list-page">
+            <h2 className="header">Artists</h2>
         <ul id="artist-list">{artists.map((artist)=>{return<div><a className="artist" href={`/artists/${artist.id}`} key={artist.id}>{artist.name}</a><br/></div>})}</ul>
         </div>
         {/* <div className="circle"></div> */}
--- src/Components/Museums.js
+++ src/Components/Museums.js
@@ -8,6 +8,7 @@ function Museums({museums,setMuseums}){
         <div className="circle-2"></div>
         <div className="circle-3"></div>
         <div className="museums-list-page">
+            <h2 className="header">Museums</h2>
         <ul id="museum-list">{museums.map((museum)=>{return<div><a className="museum" href={`/museums/${museum.id}`} key={museum.id}>{museum.name}</a><br/></div>})}</ul>
         </div>
         </div>
