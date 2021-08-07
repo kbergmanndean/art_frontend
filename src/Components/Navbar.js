eaf06025352c4c00d991d4d5644afadae00917d0
Kaia <kbergmanndean@gmail.com>
2021-08-07 18:33:36 -0400

    changed path for home button on navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,7 @@ function Navbar (){
     <div >
     <nav className="navbar navbar-expand-lg navbar-light" id="navyellow">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">Home</a>
+    <a className="navbar-brand" href="/art_frontent/#">Home</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
